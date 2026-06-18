68835e1265bcb521f89bcce1006f295f5016e1dc
Sathvic Puchalapalli <sathvic15@gmail.com>
2026-06-18 14:26:03 -0400

    Remove run.sh; use javac/java commands from README

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Compile + run the generator. Requires a JDK (javac) on PATH.
-set -e
-mkdir -p out
-javac -d out src/main/java/com/poc/testdata/TestDataGenerator.java
-java -cp out com.poc.testdata.TestDataGenerator "$@"
